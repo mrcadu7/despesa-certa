4df914330227f2b56bfc2006afa28fce66e1e051
Cadú <139708523+mrcadu7@users.noreply.github.com>
2025-07-30 23:07:18 -0300

    feat: adicionar suporte à documentação da API com drf-yasg

--- backend_expenses/backend_expenses/urls.py
+++ backend_expenses/backend_expenses/urls.py
@@ -1,20 +1,6 @@
-"""
-URL configuration for backend_expenses project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from django.contrib import admin
 from django.urls import path, include
@@ -27,9 +13,22 @@ from expenses.views import ExpenseViewSet
 router = routers.DefaultRouter()
 router.register(r'expenses', ExpenseViewSet, basename='expense')
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Despesa Certa API",
+        default_version='v1',
+        description="Documentação automática da API de despesas",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    patterns=[path('api/', include(router.urls))],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
